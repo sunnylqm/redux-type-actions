c588181336c28132619802924df7d6f293249af4
sunny.luo <sunnylqm@qq.com>
2018-12-20 17:36:00 +0800

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # redux-type-actions
+
+Make redux actions easy to write and strong typed!
+No more `"UGLY_UPPER_CASE_UNDERSCORE_ACTION"` and bring `auto complete` to the redux world!
+
+### Installation
+
+```
+yarn add redux-type-actions
+```
+
+You don't need any configurations for react-native typescript projects.
+
+Configurations for web is WIP.
+
+### Usage
---
+++ index.ts
@@ -0,0 +1,33 @@
+export function createAction<T>() {
+  return (type: string) => (payload: T) => ({ type, payload });
+}
+
+export const NoArgAction = (type: string) => () => ({ type, payload: undefined });
+export const ToggleAction = createAction<boolean>();
+export const StringAction = createAction<string>();
+export const NumberAction = createAction<number>();
+
+type TypedActions<T extends { [key: string]: (...args: any[]) => any }> = { [K in keyof T]: ReturnType<T[K]> };
+
+export function createActions<ActionsMapping extends { [key: string]: (...args: any[]) => any }>(
+  actions: ActionsMapping,
+) {
+  const typedActions: Partial<TypedActions<ActionsMapping>> = {};
+
+  let key: keyof ActionsMapping;
+  for (key in actions) {
+    const typedAction = actions[key](key);
+    typedAction.toString = () => key; // for redux-saga
+    typedActions[key] = typedAction;
+  }
+
+  return typedActions as TypedActions<ActionsMapping>;
+}
+
+type refineAction<T extends { [key: string]: (...args: any[]) => any }> = {
+  [K in keyof T]: ReturnType<T[K]> & { type: K }
+};
+
+type ValueOf<T> = T[keyof T];
+
+export type ActionValue<T extends { [key: string]: (...args: any[]) => any }> = ValueOf<refineAction<T>>;
